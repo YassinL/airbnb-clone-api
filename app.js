0e0cbbe2c0b6581195eb3b0919bdc35df873d826
YassinL <yassin.leclercq1@gmail.com>
2020-06-29 16:52:05 +0200

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen('4000', (err) => {
+app.listen('8000', (err) => {
   if (err) console.error(err);
   else console.log('Serveur Démarré');
 });
