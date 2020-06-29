c31b0da8ee231f258675bf6dca48d66b60800615
YassinL <yassin.leclercq1@gmail.com>
2020-06-29 15:58:01 +0200

    création de User

---
+++ migrations/20200629134108-create-users.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING(1000),
+      },
+      birthday: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
---
+++ models/users.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Users.init(
+    {
+      firstName: DataTypes.STRING(50),
+      lastName: DataTypes.STRING(50),
+      email: DataTypes.STRING(255),
+      password: DataTypes.STRING(255),
+      city: DataTypes.STRING(50),
+      description: DataTypes.STRING(1000),
+      birthday: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'Users',
+    }
+  );
+  return Users;
+};
